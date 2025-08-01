3d7704a51aafa64977fb6690d76db5376da874cd
Amir Hussein <amirghasemian1381@gmail.com>
2025-08-01 10:33:05 +0330

    fix: correct typo in UserUpdateBasicBase class and improve docstring formatting

--- schemas/user_schema.py
+++ schemas/user_schema.py
@@ -11,14 +11,28 @@ class UserBase(BaseModel):
     password: str
 
 
+class UserUpdateBasicBase(BaseModel):
+    """
+    This class is for updating the username and emil updating without password
+    Args:
+            BaseModel (_type_): _description_
+    """
+
+    username: str
+    email: EmailStr
+
+
 class UserResponse(BaseModel):
-	"""
-	This class is for response of the request that user is sent 
-	"""
-	user_id: int
-	username: str
-	email: EmailStr
-	password_hash: SecretStr
-	is_active: bool
-	created_at: PastDatetime
-	
+    """
+    This class is for response of the request that user is sent
+    """
+
+    user_id: int
+    username: str
+    email: EmailStr
+    password_hash: SecretStr
+    is_active: bool
+    created_at: PastDatetime
+    
+    class Config:
+        orm_mode = True

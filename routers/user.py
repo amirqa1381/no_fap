b33e6306fe31aa156c81ffc7cc9ccabba4691737
Amir Hussein <amirghasemian1381@gmail.com>
2025-08-01 10:33:14 +0330

    fix: improve formatting and spacing in user route functions

--- routers/user.py
+++ routers/user.py
@@ -7,24 +7,21 @@ from schemas.user_schema import UserResponse, UserBase
 router = APIRouter(prefix="/user", tags=["user"])
 
 
-
-
 @router.post("/", response_model=UserResponse)
-async def create_user_route(request: UserBase,db: Session = Depends(get_db)):
+async def create_user_route(request: UserBase, db: Session = Depends(get_db)):
     return create_user(db=db, request=request)
 
 
 @router.get("/all", response_model=list[UserResponse])
-async def get_all_users_route(db:Session = Depends(get_db)):
+async def get_all_users_route(db: Session = Depends(get_db)):
     """
     This function is for getting the all the users
     """
     return get_all_users(db=db)
 
 
-
 @router.get("/specific-user/{user_id}", response_model=UserResponse)
-async def get_specific_user_route(user_id: int, db:Session = Depends(get_db)):
+async def get_specific_user_route(user_id: int, db: Session = Depends(get_db)):
     """
     this function is for getting the specific user
     Args:

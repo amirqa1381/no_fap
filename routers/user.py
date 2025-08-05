c3f2152a47cca6c31aecdf83cf067c6b928d6584
Amir Hussein <amirghasemian1381@gmail.com>
2025-08-05 23:31:26 +0330

    feat: add login functionality with token generation and user verification

--- routers/user.py
+++ routers/user.py
@@ -1,7 +1,16 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordRequestForm
 from sqlalchemy.orm.session import Session
 from database.db_connection import get_db
-from database.actions.user_action import create_user, get_all_users, get_specific_user, update_specific_user
+from database.actions.user_action import (
+    create_user,
+    get_all_users,
+    get_specific_user,
+    update_specific_user,
+    get_user_by_username,
+)
+from auth.hashing import verify_password
+from auth.security import create_access_token
 from schemas.user_schema import UserResponse, UserBase, UserUpdateBasicBase
 
 router = APIRouter(prefix="/user", tags=["user"])
@@ -21,7 +30,6 @@ async def create_user_route(request: UserBase, db: Session = Depends(get_db)):
         _type_: _description_
     """
     return create_user(db=db, request=request)
-
 
 
 # getting user instances
@@ -49,8 +57,11 @@ async def get_specific_user_route(user_id: int, db: Session = Depends(get_db)):
 
 # updating the user instances
 
+
 @router.put("/update/{user_id}", response_model=UserResponse)
-async def update_user_route(user_id: int, request: UserUpdateBasicBase, db:Session = Depends(get_db)):
+async def update_user_route(
+    user_id: int, request: UserUpdateBasicBase, db: Session = Depends(get_db)
+):
     """
     This function is for updating the specific user
     Args:
@@ -61,3 +72,20 @@ async def update_user_route(user_id: int, request: UserUpdateBasicBase, db:Sessi
     return update_specific_user(id=user_id, db=db, request=request)
 
 
+# login user
+@router.post("/token")
+async def login(form_data: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(get_db)):
+    """
+    Function for login user and getting the access token
+
+    Args:
+        form_data (OAuth2PasswordRequestForm, optional): Defaults to Depends().
+        db (Session, optional): Defaults to Depends(get_db).
+    """
+    user = get_user_by_username(db=db,username=form_data.username)
+    if not user or not verify_password(form_data.password, user.password_hash):
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="incorrect username or password")
+    
+    # creating the access token
+    access_token = create_access_token(data={"sub": user.username})
+    return {"access_token": access_token, "token_type": "bearer"}

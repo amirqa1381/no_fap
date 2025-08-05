f636c5bd0204f867b30821257abe9fc9e881dc7c
Amir Hussein <amirghasemian1381@gmail.com>
2025-08-05 23:10:24 +0330

    feat: add configuration settings class for environment variable management

---
+++ auth/config.py
@@ -0,0 +1,16 @@
+from pydantic.env_settings import BaseSettings
+
+
+class Settings(BaseSettings):
+    """
+    Settings for the application, loaded from environment variables.
+
+    Args:
+        BaseSettings (_type_): Base class for settings management.
+    """
+    secret_key: str
+    algorithm: str = "HS256"
+    access_token_expire_minutes: int = 30
+
+    class Config:
+        env_file = ".env"

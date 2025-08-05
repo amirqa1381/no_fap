cebbdc3cb8f7ba9350dc607aa4b88d26bb0a5230
Amir Hussein <amirghasemian1381@gmail.com>
2025-08-05 23:18:06 +0330

    feat: implement authentication and token verification functionality

---
+++ auth/auth.py
@@ -0,0 +1,24 @@
+from fastapi.security import OAuth2PasswordBearer
+from fastapi import Depends, HTTPException, status
+from security import verify_token
+
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+
+
+def get_current_user(token: str = Depends(oauth2_scheme)):
+    """
+    Get the current user based on the provided token.
+
+    Args:
+        token (str, optional): Defaults to Depends(oauth2_scheme).
+    """
+    payload = verify_token(token)
+    if not payload:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Could not validate credentials",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+    return payload.get("sub")  # Assuming 'sub' is the user identifier in the
+    
---
+++ auth/security.py
@@ -0,0 +1,38 @@
+from jose import jwt, JWTError
+from datetime import datetime, timedelta
+from config import Settings
+from typing import Optional
+
+
+
+setting = Settings()
+
+def create_access_token(data: dict, expires_delta: Optional[timedelta] = None):
+    """
+    Creating the access token for the user if login into the system
+
+    Args:
+        data (dict): 
+        expires_delta (timedelta, optional): _description_. Defaults to None.
+    """
+    to_encode = data.copy()
+    expire = datetime.now() + (expires_delta or timedelta(minutes=15))
+    to_encode.update({"exp": expire})
+    return jwt.encode(to_encode, setting.secret_key, algorithm=setting.algorithm)
+
+
+def verify_token(token: str):
+    """
+    Verify the token for the user
+
+    Args:
+        token (str): 
+
+    Raises:
+        JWTError: Could not validate credential.
+    """
+    try:
+        payload = jwt.decode(token, setting.secret_key, algorithms=[setting.algorithm])
+        return payload
+    except JWTError:
+        raise JWTError("Could not validate credentials")

c79b2f5bf6eda9480fbf5f66365f9d6c27591be4
Amir Hussein <amirghasemian1381@gmail.com>
2025-08-09 00:58:04 +0330

    feat: add functions to retrieve, delete, and list user streaks with error handling

--- database/actions/streak_action.py
+++ database/actions/streak_action.py
@@ -27,3 +27,69 @@ def create_streak(db:Session, request: StreakBase) -> StreakResponse:
         end_date=new_streak.end_date,
         relapsed=new_streak.relapsed
     )
+    
+    
+    
+def get_streak_by_id(db: Session, streak_id: int) -> StreakResponse:
+    """
+    Function to retrieve a streak by its ID.
+
+    Args:
+        db (Session): Database session.
+        streak_id (int): ID of the streak to retrieve.
+
+    Returns:
+        StreakResponse: Response model containing the streak details.
+    """
+    streak = db.query(Streak).filter(Streak.streak_id == streak_id).first()
+    if not streak:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Streak not found")
+    
+    return StreakResponse(
+        streak_id=streak.streak_id,
+        user_id=streak.user_id,
+        start_date=streak.start_date,
+        end_date=streak.end_date,
+        relapsed=streak.relapsed
+    )
+    
+    
+def get_all_streaks(db: Session, user_id: int) -> list[StreakResponse]:
+    """
+    Function to retrieve all streaks for a user.
+
+    Args:
+        db (Session): Database session.
+        user_id (int): ID of the user whose streaks are to be retrieved.
+
+    Returns:
+        list[StreakResponse]: List of streaks associated with the user.
+    """
+    streaks = db.query(Streak).filter(Streak.user_id == user_id).all()
+    
+    return [StreakResponse(
+        streak_id=streak.streak_id,
+        user_id=streak.user_id,
+        start_date=streak.start_date,
+        end_date=streak.end_date,
+        relapsed=streak.relapsed
+    ) for streak in streaks]
+    
+
+def delete_streak(db: Session, streak_id: int) -> None:
+    """
+    Function to delete a streak by its ID.
+
+    Args:
+        db (Session): Database session.
+        streak_id (int): ID of the streak to delete.
+
+    Raises:
+        HTTPException: If the streak is not found.
+    """
+    streak = db.query(Streak).filter(Streak.streak_id == streak_id).first()
+    if not streak:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Streak not found")
+    
+    db.delete(streak)
+    db.commit()

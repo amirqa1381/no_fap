559a4f74e5bfbdf1d91986812ab187efcb993b86
Amir Hussein <amirghasemian1381@gmail.com>
2025-08-17 23:13:25 +0330

    feat: include streak and journal routers in main application

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from fastapi import FastAPI
-from routers import user
+from routers import user, streak, journal
 
 
 app = FastAPI()
 app.include_router(user.router)
+app.include_router(streak.router)
+app.include_router(journal.router)
 
 
 @app.get("/")

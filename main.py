e093be423788da5851286cce51e77fd665df56c4
Amir Hussein <amirghasemian1381@gmail.com>
2025-08-29 10:47:12 +0330

    feat: include comment router in main application

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-from routers import user, streak, journal, post
+from routers import user, streak, journal, post, comment
 
 
 app = FastAPI()
@@ -7,6 +7,8 @@ app.include_router(user.router)
 app.include_router(streak.router)
 app.include_router(journal.router)
 app.include_router(post.router)
+app.include_router(comment.router)
+
 
 @app.get("/")
 async def index():
